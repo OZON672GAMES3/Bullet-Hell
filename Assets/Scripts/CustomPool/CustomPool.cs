d26aab9562a7013378896defbf9a2e2c843bdcd0
schematics <dmitrijkrivoguzov@gmail.com>
2025-03-04 21:02:07 +0700

    added object pool

--- Assets/Scripts/CustomPool/CustomPool.cs
+++ Assets/Scripts/CustomPool/CustomPool.cs
@@ -4,5 +4,47 @@ using UnityEngine;
 
 namespace CustomPool
 {
-    
+    public class CustomPool<T> where T : MonoBehaviour
+    {
+        private T _prefab;
+        private List<T> _objects;
+
+        public CustomPool(T prefab, int prewarmObjects)
+        {
+            _prefab = prefab;
+            _objects = new List<T>();
+
+            for (int i = 0; i < prewarmObjects; i++)
+            {
+                var obj = GameObject.Instantiate(_prefab);
+                obj.gameObject.SetActive(false);
+                _objects.Add(obj);
+            }
+        }
+
+        public T Get()
+        {
+            var obj = _objects.FirstOrDefault(x => !x.isActiveAndEnabled);
+
+            if (!obj)
+            {
+                obj = Create();
+            }
+
+            obj.gameObject.SetActive(true);
+            return obj;
+        }
+
+        public void Release(T obj)
+        {
+            obj.gameObject.SetActive(false);
+        }
+
+        private T Create()
+        {
+            var obj = GameObject.Instantiate(_prefab);
+            _objects.Add(obj);
+            return obj;
+        }
+    }
 }
